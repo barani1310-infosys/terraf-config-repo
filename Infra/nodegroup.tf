7c18ab3a252e1d785f66e8f32b5d5534d5f8e640
barani1310-infosys <baranidharan@infosys.com>
2025-04-03 13:16:27 +0530

    add

---
+++ Infra/nodegroup.tf
@@ -0,0 +1,32 @@
+# EKS Node Groups
+resource "aws_eks_node_group" "nodes" {
+  for_each = var.node_groups
+
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = each.key
+  node_role_arn   = aws_iam_role.nodes.arn
+  subnet_ids      = aws_subnet.private[*].id
+
+  scaling_config {
+    desired_size = each.value.desired_size
+    max_size     = each.value.max_size
+    min_size     = each.value.min_size
+  }
+
+  instance_types = each.value.instance_types
+  ami_type       = each.value.ami_type
+  disk_size      = each.value.disk_size
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.cluster_name}-${each.key}-node-group"
+    }
+  )
+
+  depends_on = [
+    aws_iam_role_policy_attachment.nodes_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.nodes_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.nodes_AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
---
+++ Infra/variables.tf
@@ -0,0 +1,55 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+ 
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.28"
+}
+
+variable "tags" {
+  description = "Additional tags for all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "node_groups" {
+  description = "Map of node group configurations"
+  type = map(object({
+    desired_size   = number
+    max_size       = number
+    min_size       = number
+    instance_types = list(string)
+    ami_type       = string
+    disk_size      = number
+  }))
+  default = {}
+}
